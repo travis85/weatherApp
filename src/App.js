064b50a0fe0aa7b8fac291923306bb196ab5abd1
travis flake <tflake83@gmail.com>
2022-09-06 08:36:35 -0400

    placed functions in seperate files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { useState } from 'react';
-import rainPic from './assets/rainPic.jpeg'
-import sunPic from './assets/sunPic.webp'
-import snowPic from './assets/snowPic.jpeg'
-import cloudPic from './assets/cloudPic.jpeg'
-import mistPhoto from './assets/mistPhoto.webp'
-import thunderstormPic from './assets/thunderstormPic.jpeg'
+import React, { useState, useEffect } from 'react'
 import Map from './components/Map';
+import DateBuilder from './utils/DateBuilder'
+import weatherSet from './utils/weatherSet'
 
 const weatherApi = {
-  key: "d21dbf4ec7c07da49bc079f1518d953d",//TODO ,env
+  key: 'd21dbf4ec7c07da49bc079f1518d953d',//TODO ,env
   base: "https://api.openweathermap.org/data/2.5/"
 }
+
 const geoCodeApi ={
-  key:"d21dbf4ec7c07da49bc079f1518d953d",
+  key: 'd21dbf4ec7c07da49bc079f1518d953d',
   base: "https://api.openweathermap.org/geo/1.0/"
 }
+
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
@@ -42,87 +40,78 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    
+  }, [])
 
-  const dateBuilder = (d) => {
-    let months = ['January','february','March','April','May','June','July','August','September','October','November','December'];
-    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`
-  }
-
-  const weatherSet = (weather) => {
-    const weatherStyle = document.querySelector('.app')
-    // const computedStyle = window.getComputedStyle(weatherStyle)
-    if (weather === 'Rain') {
-      weatherStyle.style.setProperty('background-image',`url(${rainPic})`)
-    } else if (weather === 'Clear') {
-      weatherStyle.style.setProperty('background-image',`url(${sunPic})`)
-    } else if (weather === 'Snow') {
-      weatherStyle.style.setProperty('background-image',`url(${snowPic})`)
-    } else if (weather === 'Clouds') {
-      weatherStyle.style.setProperty('background-image',`url(${cloudPic})`)
-    } else if (weather === 'Thunderstorm') {
-      weatherStyle.style.setProperty('background-image',`url(${thunderstormPic})`)
-    }else if (weather === 'Mist') {
-      weatherStyle.style.setProperty('background-image',`url(${mistPhoto})`)
-    }
-  }
 
 
   return (
     <>
     <div className="app ">
       {/* MOBILE VIEW */}
-      <div className=' md:hidden'>
-      <main className='grid grid-cols-1'>
+      <main className='grid grid-cols-1 md:hidden '>
         <div className=''>
           <div className="search-box mt-7">
             <input type="text" 
             className='search-bar mt-7 ' 
             placeholder='Enter City...'
-            onChange={e => setQuery(e.target.value)}
             value={query}
+            onChange={e => setQuery(e.target.value)}
+           
             onKeyUp={search}
             />
         </div>
 
         { weather.timezone && 
           
-          <div>
+          <div className=''>
             <div className="locationBox">
               <div className="location">{geoCode[0].name}, {geoCode[0].state} </div>
-              {/* <div className='population'>Poulation: {new Intl.NumberFormat().format(weather.city.population)}</div> */}
-              <div className="date">{ dateBuilder(new Date()) }</div>
+              <div className="date">{ DateBuilder(new Date()) }</div>
             </div>
             <div className='weatherBox'>
             <div className='temp'>
               {Math.round(weather.current.temp)}°F
             </div>
             <div className='weather '>
-              { weatherSet(weather.current.weather[0].main)}
+              { weatherSet(weather.current.weather[0].main), weather.current.weather[0].main}
             </div>
             </div>
           </div>
           }
+          </div>
+          
+          { weather.timezone && 
+        <div className=''>
+          <ul>
+            {weather.daily.slice(1,7).map((day)=>{
+               return(
+                
+                <div className='forcastSection px-1 py-2 items-center flex flex-wrap max-w-[75%]'>
+                  <li>
+                    <div className='forcastSection px-1 py-2 flex items-center'>{Math.round(day.temp.max)}°F</div>
+                     <div>{DateBuilder(new Date(day.dt * 1000))}</div>
 
+                  </li>
+                </div>
+               )
+            })}
+          </ul>
         </div>
+        }     
 
 
     
       </main>
 
-      </div>
 
       {/* DESKTOP VIEW */}
 
       <main className='hidden md:grid grid-cols-3  '>
            
-        <div className=''>
+        <div className='grid grid-cols-1 '>
            
           <div className="search-box ">
             <input type="text" 
@@ -132,63 +121,52 @@ function App() {
             value={query}
             onKeyUp={search}
             />
-         
           </div>
+            
           { weather.timezone && 
-          
           <div className=''>
             <div className="locationBox">
               <div className="location">{geoCode[0].name}, {geoCode[0].state} </div>
-              <div className="date">{ dateBuilder(new Date()) }</div>
+                  <div className="date">{DateBuilder(new Date())}</div>
+
             </div>
             <div className='weatherBox'>
               <div className='temp'>
                 {Math.round(weather.current.temp)}°F
               </div>
               <div className='weather '>
-                {weather.current.weather[0].main}
+                {weatherSet(weather.current.weather[0].main),weather.current.weather[0].main}
               </div>
             </div>
-              
           </div>
-          
           }   
-          </div>
-            < Map lat={weather.lat} lng={ weather.lon}/>
-           
-       
-              
-           
-                    
-
+        </div>
+          
+        <div className='flex items-center'>
+            < Map lat={Number(weather.lat)} lng={ Number(weather.lon)}/>
+        </div>
  
-      { weather.timezone && 
-
+        { weather.timezone && 
         <div className='flex justify-end'>
-
           <ul>
-            {weather.daily.slice(1,8).map((day)=>{
+            {weather.daily.slice(1,7).map((day)=>{
                return(
                 
                 <div className='forcastSection px-1 py-2 items-center'>
                   <li>
                     <div className='forcastSection px-1 py-2 flex items-center'>{Math.round(day.temp.max)}°F</div>
-                    <div>{dateBuilder(new Date(day.dt * 1000))}</div>
-                   
+                     <div>{DateBuilder(new Date(day.dt * 1000))}</div>
 
                   </li>
                 </div>
                )
-           })}
+            })}
           </ul>
         </div>
-      
         }     
-
-    
       </main>
-      </div>
-      </>
+    </div>
+    </>
   );
 }
 
