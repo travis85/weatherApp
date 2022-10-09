3f6e5629c24c9d1ae8ce7b11b6277a94b1649d00
travis flake <tflake83@gmail.com>
2022-10-09 17:11:53 -0400

    error handling  register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,28 +2,37 @@ import { useState } from 'react'
 import React from 'react'
 import { auth } from '../utils/firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
-// import { collection, addDoc, onSnapshot, deleteDoc, doc, where, query, updateDoc, connectFirestoreEmulator} from "firebase/firestore"; 
-
+import loginValidation from './loginValidationRules'
 
 
 export default  function Register(props) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    // const [userName, setUserName] = useState('')
+    const [errors, setErrors] = useState([])
 
-    // const [user, setUser] = useState({
-    //     email: '',
-    //     uid: '',
-    //     userName: '',
-    // })
+    async function handleRegister() {
+        const values = {
+            email: email,
+            password: password
+        }
+        const errorsArray = Object.keys(loginValidation(values))
+        
+        if (errorsArray.length > 0){
+            const newArray = Object.values(loginValidation(values))
+            setErrors(errors => [...errors, newArray])
+            clearError()
+        } else {
+            const res = await createUserWithEmailAndPassword(auth, email, password) 
+            setEmail('')
+            setPassword('')
+            alert('Your Registered, You can now sign in!')
+        }
+    }
 
-    async function handleRegister(){
-        // setShowRegister(false)
-        const res = await createUserWithEmailAndPassword(auth, email, password) 
-        setEmail('')
-        setPassword('')
-        alert('Your Registered, You can now sign in!')
-        console.log(res)
+    function clearError() {
+        setTimeout(() => {
+        setErrors([])
+        },3000)
     }
 
     function updateEmail(event){
@@ -33,50 +42,36 @@ export default  function Register(props) {
         setPassword(event.target.value)
     }
 
-    // const onClickHandler = async () =>{
-    //     const collectionRef = collection(firestore, "users");
-    //     const payload = { note: note, uid:user.uid }
-    //     await addDoc( collectionRef, payload );
-    //     setNote('');
-    // }
-
-
-    
-
-    // function handleLogOut(){
-    //     setUser({
-    //     email: '',
-    //     uid: '',
-    //     userName: '',
-    //     })
-    //     alert('Your logged out')
-
-    // }
-
-
-
-
-  return (
-      
-
-    <div>
-        <input
-            className='bg-gray-400 border-white border-solid px-4 py-2 rounded placeholder-white'
-            type=' text'
-            placeholder='Register Email'
-            value={email}
-            onChange={updateEmail}>
-        </input>
-        <input
-            className='bg-gray-400 border-white border-solid px-4 py-2 m-4 placeholder-white rounded'
-            type='password'
-            placeholder='Register Password'
-            value={password}
-            onChange={updatePassword}>
-        </input>
-        <button className='text-white hover:text-slate-400' onClick={()=> (handleRegister())}>Register</button>
-    </div>
-  )
+    return (
+        <>
+        <div>
+            <input
+                className='bg-gray-400 border-white border-solid px-4 py-2 rounded placeholder-white'
+                type=' text'
+                placeholder='Register Email'
+                value={email}
+                onChange={updateEmail}>
+            </input>
+            <input
+                className='bg-gray-400 border-white border-solid px-4 py-2 m-4 placeholder-white rounded'
+                type='password'
+                placeholder='Register Password'
+                value={password}
+                onChange={updatePassword}>
+            </input>
+            <button className='text-white hover:text-slate-400' onClick={()=> (handleRegister())}>Register</button>
+        </div>
+        <ul>
+            {errors.flat(1).map((error,index) => {
+                return (
+                    <li key={index}>
+                        {error}
+                    </li> 
+                )
+            })}
+        </ul>
+        </>
+    )
 }
 
 
