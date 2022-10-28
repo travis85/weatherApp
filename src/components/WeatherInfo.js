d815324e74a3c575c0b6d7f89a4796b68e87a761
travis flake <tflake83@gmail.com>
2022-10-28 16:39:42 -0400

    .env added to key

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -11,20 +11,20 @@ function App(props) {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
   const [geoCode, setGeoCode] = useState({});
+
+
   
   const weatherApi = {
-    key: 'd21dbf4ec7c07da49bc079f1518d953d',//TODO ,env
+    key: process.env.REACT_APP_WEATHER_KEY,//TODO ,env
     base: "https://api.openweathermap.org/data/2.5/"
   }
 
   const geoCodeApi ={
-    key: 'd21dbf4ec7c07da49bc079f1518d953d',
+    key: process.env.REACT_APP_WEATHER_KEY,
     base: "https://api.openweathermap.org/geo/1.0/"
   }
-
   
-  //USED TO SET DEFAULT CITY ON PAGE LOAD
-  useEffect(() => {
+  const setInitialWeather = () => {
     fetch(`${geoCodeApi.base}direct?q=romulus&limit=1&appid=${geoCodeApi.key}`)
     .then(res => res.json())
     .then(result => {
@@ -38,11 +38,17 @@ function App(props) {
       setQuery('')
       console.log(result)
     });
+  }
+  
+  //USED TO SET DEFAULT CITY ON PAGE LOAD
+  useEffect(() => {
+   setInitialWeather()    
   },[])
 
 
   const search = evt => {
-    if(evt.key === "Enter"){
+    if (evt.key === "Enter") {
+      
       fetch(`${geoCodeApi.base}direct?q=${query}&limit=1&appid=${geoCodeApi.key}`)
       .then(res => res.json())
       .then(result => {
@@ -54,29 +60,13 @@ function App(props) {
       .then(result => {
         setWeather(result)
         setQuery('')
-       
-      });
+      })
     }
   }
 
-   function fetchTemp(city){
-    fetch(`${geoCodeApi.base}direct?q=${city}&limit=1&appid=${geoCodeApi.key}`)
-    .then(res => res.json())
-    .then(result => {
-      console.log(result, 'fetchTemp')
-      fetch(`${weatherApi.base}onecall?lat=${result[0].lat}&lon=${result[0].lon}&units=imperial&appid=${weatherApi.key}`)
-    })
-    .then(res => res.json())
-    .then(result => {
-      console.log(Math.round(result.current.temp), 'fecthTemp - WeatherInfo.js')
-        return Math.round(result.current.temp)
-      
-    });
-  }
-  
 
 
-
+   
   return (
     <>
       
@@ -92,7 +82,7 @@ function App(props) {
             onChange={e => setQuery(e.target.value)}
             onKeyUp={search}
           />
-        </div>
+          </div>
 
         { weather.timezone && 
           
@@ -120,7 +110,7 @@ function App(props) {
             {weather.daily.slice(1,7).map((day)=>{
                return(
                 
-                <div className='forcastSection px-1 py-2 items-center flex flex-wrap max-w-[75%]'>
+                <div className='forcastSection px-1 py-2 items-center flex flex-wrap max-w-[75%]' key={day.dt}>
                   <li>
                     <div className='forcastSection px-1 py-2 flex items-center'>{Math.round(day.temp.max)}°F</div>
                      <div>{DateBuilder(new Date(day.dt * 1000))}</div>
@@ -135,24 +125,15 @@ function App(props) {
       </main>
 
 
-      {/* DESKTOP VIEW */}
-      <Banner fetchTemp={fetchTemp} />
-      <main className='hidden md:grid grid-cols-3  '>
+        {/* DESKTOP VIEW */}
+        {/* PASS GEO CODE API AND PLACE FUNCTION IN BANNER COMPONENT */}
+      <Banner />
+      <main className='hidden md:grid grid-cols-3 relative'>
           
         <div className='grid grid-cols-1 '>
            
-          <div className="search-box ">
-            <input type="text" 
-              className='search-bar ' 
-              placeholder='Enter City...'
-              onChange={e => setQuery(e.target.value)}
-              value={query}
-              onKeyUp={search}
-            />
-          </div>
-            
           { weather.timezone && 
-          <div>
+          <div className='grid content-center'>
             <div className="locationBox">
               <div className="location">{geoCode[0].name}, {geoCode[0].state} </div>
                 <div className="date">{DateBuilder(new Date())}</div>
@@ -171,25 +152,31 @@ function App(props) {
         </div>
           
         {/* MAP DIV */}
-        <div className='flex items-center'> 
+          <div className='flex items-center'> 
+            <div className="search-box absolute top-2 pl-20">
+            <input type="text" 
+              className='search-bar ' 
+              placeholder='Enter City...'
+              onChange={e => setQuery(e.target.value)}
+              value={query}
+              onKeyUp={search}
+            />
+          </div>
           < Map lat={Number(weather.lat)} lng={Number(weather.lon)} />
         </div>
           
         {/* ITERATING OVER ARRAY AND SEPERATING  */}
-          {weather.timezone && 
-            
-        <div className='flex justify-end'>
-              <ul>
-                
-              {weather.daily.slice(1, 7).map((day) => {
-              
-               return(
-                <div className='forcastSection px-1 py-2 items-center'>
-                  <li>
-                    <div className='forcastSection px-1 py-2 flex items-center'>{Math.round(day.temp.max)}°F</div>
-                    <div>{DateBuilder(new Date(day.dt * 1000))}</div>
-                  </li>
-                </div>
+        {weather.timezone && 
+        <div className='grid grid-cols-1 w-32 place-self-center'>
+          <ul>
+            {weather.daily.slice(1, 7).map((day) => {
+              return(
+              <div className='forcastSection px-1 py-2 items-center' key={day.dt}>
+                <li>
+                  <div className='forcastSection px-1 py-2 flex items-center'>{Math.round(day.temp.max)}°F</div>
+                  <div>{DateBuilder(new Date(day.dt * 1000)).split(' ').slice(0, 2).join(' ')}</div>
+                </li>
+              </div>
                )
             })}
           </ul>
